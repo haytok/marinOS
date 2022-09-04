69b94893e4f052c3a27d78bd79b20b9f3b161cdb
haytok <simplelpmis6@gmail.com>
2022-09-05 00:44:43 +0900

    fixed main.c

--- src/03/bootload/main.c
+++ src/03/bootload/main.c
@@ -3,6 +3,8 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+
 int main(void)
 {
 	// シリアルデバイスの初期化
@@ -11,6 +13,12 @@ int main(void)
 	puts("Hello World :)\n");
 	putxval(0x0a0, 3);
 	PRINT_NEWLINE();
+
+	// 検証 (変数 value が書き換えられないことを確認する)
+	putxval(value, 0);
+	PRINT_NEWLINE();
+	value = 11;
+	putxval(value, 0);
 
 	while (1)
 		;
