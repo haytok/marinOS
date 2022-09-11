86624e070208129f7bb3ca5c76d1849ce80d9485
haytok <simplelpmis6@gmail.com>
2022-09-12 01:12:25 +0900

    updted to accept for XMODEM protocol

--- src/04/bootload/main.c
+++ src/04/bootload/main.c
@@ -2,6 +2,7 @@
 #include "defines.h"
 #include "serial.h"
 #include "lib.h"
+#include "xmodem.h"
 
 volatile int value_1;
 volatile int value = 10;
@@ -23,28 +24,75 @@ static int init(void)
 	return 0;
 }
 
+static void wait()
+{
+	volatile long i;
+	for (i = 0; i < 300000; i++)
+		;
+}
+
+// メモリの 16 進ダンプ
+static int dump(char *buf, long size)
+{
+	if (size < 0) {
+		puts("no data :(\n");
+		return -1;
+	}
+
+	long i;
+	for (i = 0; i < size; i++) {
+		putxval(buf[i], 2);
+		if ((i & 0xf) == 15) {
+			puts("\n");
+		} else {
+			// hexdump みたいに横に 16 個の 16 進数を出力するための処理を挟んでいる。
+			if ((i & 0xf) == 7)
+				puts(" ");
+			puts(" ");
+		}
+	}
+	puts("\n");
+
+	return 0;
+}
+
 int main(void)
 {
+	unsigned char buf[16];
+	static unsigned char *loadbuf = NULL;
+	// VAIO 側から受け取ったデータを保存する RAM の領域のアドレスを保持する。
+	extern int buffer_start;
+	static long size = -1;
+
+	// extern int data_start;
+	// int *p;
+	// p = &data_start;
+
 	init();
 
-	puts("Hello World :)\n");
-	putxval(0x0a0, 3);
-	PRINT_NEWLINE();
-
-	// 検証 (変数 value が書き換えられないことを確認する)
-	putxval(value, 0);
-	PRINT_NEWLINE();
-	value = 11;
-	putxval(value, 0);
-	PRINT_NEWLINE();
-
-	unsigned char buf[16];
+	puts("marinos boot loader started ... :)\n");
 
 	while (1) {
 		// VAIO 側では以下の puts() 関数が実行されてから、処理の受け取り待ちの状態になる。
 		puts("marinos> ");
 		gets(buf);
-		if (strncmp(buf, "echo", 4) == 0) {
+
+		if (!strcmp(buf, "load")) {
+			loadbuf = (char *)(&buffer_start);
+			size = xmodem_recv(loadbuf);
+			// 転送アプリケーションが終了して、端末アプリケーションに制御が戻るまで待ち合わせる。
+			wait();
+			if (size < 0) {
+				puts("\nXMODEM receive error :(\n");
+			} else {
+				puts("\nXMODEM receive succeeded :)\n");
+			}
+		} else if (!strcmp(buf, "dump")) {
+			puts("size: ");
+			putxval(size, 0);
+			puts("\n");
+			dump(loadbuf, size);
+		} else if (strncmp(buf, "echo", 4) == 0) {
 			puts(buf + 4);
 			PRINT_NEWLINE();
 		} else {
