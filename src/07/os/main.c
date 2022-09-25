2a6fd8ddc5af2e03b7bfcd6ca0248e828061dec2
haytok <simplelpmis6@gmail.com>
2022-09-25 16:04:02 +0900

    updated features for os

--- src/07/os/main.c
+++ src/07/os/main.c
@@ -1,27 +1,49 @@
 #include "defines.h"
 #include "serial.h"
 #include "lib.h"
+#include "interrupt.h"
+#include "intr.h"
+
+static void intr(softvec_type_t type, unsigned long sp)
+{
+	int c;
+	static char buf[32];
+	static int len = 0;
+
+	c = getc();
+
+	if (c != '\n') {
+		buf[len++] = c;
+	} else {
+		buf[len++] = '\0';
+		if (!strncmp(buf, "echo", 4)) {
+			puts(buf + 4);
+			PRINT_NEWLINE();
+		} else {
+			puts("unknown.\n");
+		}
+		puts("marinos> ");
+		len = 0;
+	}
+}
 
 int main(void)
 {
-	static char buf[32];
+	INTR_DISABLE;
 
 	puts("Boot Succeeded.\n");
 	puts("Started marinOS ...\n");
 	puts("Hello World :)\n");
 
-	while (1) {
-		puts("marinos> ");
-		gets(buf);
+	softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
+	serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
 
-		if (!strncmp(buf, "echo", 4)) {
-			puts(buf + 4);
-			PRINT_NEWLINE();
-		} else if (!strncmp(buf, "exit", 4)) {
-			break;
-		} else {
-			puts("unknown.\n");
-		}
+	puts("marinos> ");
+
+	INTR_ENABLE;
+
+	while (1) {
+		asm volatile("sleep");
 	}
 
 	return 0;
